cce21510cb3682006c40488f23f872d2c909e5e7
daws11 <dawskutel@gmail.com>
2023-04-02 17:03:03 +0700

    tampilan dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,24 +1,351 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
+import 'package:shantika_bus/conponents/search_bar.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   Dashboard({super.key});
 
-  final user = FirebaseAuth.instance.currentUser!;
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
 
+class _DashboardState extends State<Dashboard> {
+  final user = FirebaseAuth.instance.currentUser!;
+  int _selectedIndex = 0;
+
+  List<Widget> tabItems = [
+    Center(child: TicketView()),
+    Center(child: Text("Ticket")),
+    Center(child: Text("Payment")),
+    Center(child: Text("Schedule")),
+  ];
   void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        actions: [IconButton(onPressed: signUserOut, icon: Icon(Icons.logout))],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(63, 81, 181, 1),
+        ),
+        body: Center(
+          child: tabItems[_selectedIndex],
+        ),
+        bottomNavigationBar: FlashyTabBar(
+          animationCurve: Curves.linear,
+          selectedIndex: _selectedIndex,
+          iconSize: 30,
+          showElevation: false, // use this to remove appBar's elevation
+          onItemSelected: (index) => setState(() {
+            _selectedIndex = index;
+          }),
+          items: [
+            FlashyTabBarItem(
+              icon: Icon(Icons.house),
+              title: Text('Dashboard'),
+            ),
+            FlashyTabBarItem(
+              icon: Icon(Icons.airplane_ticket),
+              title: Text('Ticket'),
+            ),
+            FlashyTabBarItem(
+              icon: Icon(Icons.payment),
+              title: Text('Payment'),
+            ),
+            FlashyTabBarItem(
+              icon: Icon(Icons.schedule),
+              title: Text('Schedule'),
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text("Halo ${user.email!}"),
+    );
+  }
+}
+
+class TicketView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24))),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "BDG",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.indigo),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          color: Colors.indigo.shade50,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: SizedBox(
+                        height: 8,
+                        width: 8,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                              color: Colors.indigo.shade400,
+                              borderRadius: BorderRadius.circular(5)),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Stack(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (context, constraints) {
+                                  return Flex(
+                                    children: List.generate(
+                                        (constraints.constrainWidth() / 6)
+                                            .floor(),
+                                        (index) => SizedBox(
+                                              height: 1,
+                                              width: 3,
+                                              child: DecoratedBox(
+                                                decoration: BoxDecoration(
+                                                    color:
+                                                        Colors.grey.shade300),
+                                              ),
+                                            )),
+                                    direction: Axis.horizontal,
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                  );
+                                },
+                              ),
+                            ),
+                            Center(
+                                child: Transform.rotate(
+                              angle: 1.5,
+                              child: Icon(
+                                Icons.arrow_circle_up,
+                                color: Colors.indigo.shade300,
+                                size: 24,
+                              ),
+                            ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          color: Colors.pink.shade50,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: SizedBox(
+                        height: 8,
+                        width: 8,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                              color: Colors.pink.shade400,
+                              borderRadius: BorderRadius.circular(5)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      "JKT",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.pink),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(
+                        width: 100,
+                        child: Text(
+                          "Bandung",
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        )),
+                    Text(
+                      "2H 30M",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    SizedBox(
+                        width: 100,
+                        child: Text(
+                          "Jakarta",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "08:00 AM",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "02:30 PM",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "1 May 2020",
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          "Bus No : ",
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                        Text(
+                          "23",
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        color: Colors.grey.shade200),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Flex(
+                          children: List.generate(
+                              (constraints.constrainWidth() / 10).floor(),
+                              (index) => SizedBox(
+                                    height: 1,
+                                    width: 5,
+                                    child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey.shade400),
+                                    ),
+                                  )),
+                          direction: Axis.horizontal,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10)),
+                        color: Colors.grey.shade200),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 16, right: 16, bottom: 12),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(24),
+                    bottomRight: Radius.circular(24))),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: Colors.amber.shade50,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Icon(Icons.directions_bus_filled, color: Colors.amber),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text("Shantika Airbus",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey)),
+                Expanded(
+                    child: Text("RP.80.000,-",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
