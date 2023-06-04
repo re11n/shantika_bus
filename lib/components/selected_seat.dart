9412eed6f44ec11ae1e1e57737d32fbdaa2d6997
re11n <97107417+re11n@users.noreply.github.com>
2023-06-04 22:53:08 +0700

    nambahin const setiap file

--- lib/components/selected_seat.dart
+++ lib/components/selected_seat.dart
@@ -1,31 +1,32 @@
+// ignore: depend_on_referenced_packages
 import 'package:get/get.dart';
 
 class SelectedSeat extends GetxController {
   var indexKursi = 0.obs;
-  
+
   var kursi = List.generate(
     2,
     (indexB) => List<Map<String, dynamic>>.generate(
       24,
-      (indexK){
+      (indexK) {
         if (indexB == 0) {
           if (indexK >= 24 && indexK <= 26 || indexK >= 40 && indexK <= 44) {
             return {
               "id": "ID-${indexB + 1}-${indexK + 1}",
               "stat": "filled",
             };
-          } else{
+          } else {
             return {
               "id": "ID-${indexB + 1}-${indexK + 1}",
               "stat": "available",
             };
-          };
+          }
         } else {
           return {
             "id": "ID-${indexB + 1}-${indexK + 1}",
             "stat": "available",
-          }; 
-        }  
+          };
+        }
       },
     ),
   ).obs;
